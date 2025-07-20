02b2cf712bbf2c16bff829952208e5ba73ec98a1
Siddharth Perumalla <siddharthperumalla@mac.lan>
2025-07-19 21:58:45 -0400

    quick fix

--- quiz.js
+++ quiz.js
@@ -242,7 +242,7 @@ function displayResults(result) {
 </svg>
                     </a>
                     <button class="share-button copy-link-btn" onclick="copyShareableLink('${shareableLink}')" title="Copy link">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
                             <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
                         </svg>
@@ -271,8 +271,8 @@ function copyShareableLink(link) {
         if (copyBtn) {
             const originalTitle = copyBtn.getAttribute('title');
             copyBtn.setAttribute('title', 'Copied!');
-            copyBtn.style.background = '#10b981';
-            copyBtn.style.borderColor = '#10b981';
+            copyBtn.style.background = '#3b82f6';
+            copyBtn.style.borderColor = '#3b82f6';
             
             setTimeout(() => {
                 copyBtn.setAttribute('title', originalTitle);
